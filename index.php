dd9c5db90b685b17ccf2881cc3946c57b793298f
OliverKkk <46579765+OliverKkk@users.noreply.github.com>
2021-01-11 15:06:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Techgenie Kenya</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<a href="index.html" class="logo"><strong>TGK</strong></a>
+				<nav>
+					<a href="#menu">Menu</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h1>Your IT Solutions Center<a href=""></a></h1>
+					<ul class="actions">
+						<li><a href="#one" class="button alt scrolly big">Who we do</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<article id="one" class="post style1">
+				<div class="image">
+					<img src="images/pic14.jpg" alt="" data-position="75% center" />
+				</div>
+				<div class="content">
+					<div class="inner">
+						<header>
+							<h2><a href="generic.html">Website development</a></h2>
+							<p class="info">Fast and responsive <a href="#">websites</a></p>
+						</header>
+						<p>For creating an optimum web experience, we have developers for every top web development technology. Our forte is creating scalable and futuristic websites, using the top web development technologies, and best web development practices.</p>
+						<ul class="actions">
+							<li><a href="web-development-kenya-nairobi.html" class="button alt">Read More</a></li>
+						</ul>
+					</div>
+					<div class="postnav">
+						<a href="#" class="prev disabled"><span class="icon fa-chevron-up"></span></a>
+						<a href="#two" class="scrolly next"><span class="icon fa-chevron-down"></span></a>
+					</div>
+				</div>
+			</article>
+
+		<!-- Two -->
+			<article id="two" class="post invert style1 alt">
+				<div class="image">
+					<img src="images/pic13.jpg" alt="" data-position="10% center" />
+				</div>
+				<div class="content">
+					<div class="inner">
+						<header>
+							<h2><a href="generic.html">Digital Marketing</a></h2>
+							<p class="info">3 days ago by <a href="#">Jane Doe</a></p>
+						</header>
+						<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+						<ul class="actions">
+							<li><a href="generic.html" class="button alt">Read More</a></li>
+						</ul>
+					</div>
+					<div class="postnav">
+						<a href="#one" class="scrolly prev"><span class="icon fa-chevron-up"></span></a>
+						<a href="#three" class="scrolly next"><span class="icon fa-chevron-down"></span></a>
+					</div>
+				</div>
+			</article>
+
+		<!-- Three -->
+			<article id="three" class="post style2">
+				<div class="image">
+					<img src="images/pic12.jpg" alt="" data-position="80% center" />
+				</div>
+				<div class="content">
+					<div class="inner">
+						<header>
+							<h2><a href="generic.html">Graphic design and Print</a></h2>
+							<p class="info">3 days ago by <a href="#">Jane Doe</a></p>
+						</header>
+						<p>xxxxxxxxxxxxxxxxxxxxxxxxxx</p>
+						<ul class="actions">
+							<li><a href="generic.html" class="button alt">Read More</a></li>
+						</ul>
+					</div>
+					<div class="postnav">
+						<a href="#two" class="scrolly prev"><span class="icon fa-chevron-up"></span></a>
+						<a href="#four" class="scrolly next"><span class="icon fa-chevron-down"></span></a>
+					</div>
+				</div>
+			</article>
+
+		<!-- Four -->
+			<article id="four" class="post invert style2 alt">
+				<div class="image">
+					<img src="images/pic14.jpg" alt="" data-position="60% center" />
+				</div>
+				<div class="content">
+					<div class="inner">
+						<header>
+							<h2><a href="generic.html">Hardware Maintenance</a></h2>
+							<p class="info">Repairs, upgrades, troubleshooting <a href="#">by qualified professionals</a></p>
+						</header>
+						<p>xxxxxxxxxxxxxxxxxxxxxx</p>
+						<ul class="actions">
+							<li><a href="generic.html" class="button alt">Read More</a></li>
+						</ul>
+					</div>
+					<div class="postnav">
+						<a href="#three" class="scrolly prev"><span class="icon fa-chevron-up"></span></a>
+						<a href="#five" class="scrolly next"><span class="icon fa-chevron-down"></span></a>
+					</div>
+				</div>
+			</article>
+
+		<!-- Five -->
+			<article id="five" class="post style3">
+				<div class="image">
+					<img src="images/pic13.jpg" alt="" data-position="5% center" />
+				</div>
+				<div class="content">
+					<div class="inner">
+						<header>
+							<h2><a href="generic.html">End-User Training and Support</a></h2>
+							<p class="info">3 days ago by <a href="#">Jane Doe</a></p>
+						</header>
+						<p>xxxxxxxxxxxx</p>
+						<ul class="actions">
+							<li><a href="generic.html" class="button alt">Read More</a></li>
+						</ul>
+					</div>
+					<div class="postnav">
+						<a href="#four" class="scrolly prev"><span class="icon fa-chevron-up"></span></a>
+						<a href="#six" class="scrolly next"><span class="icon fa-chevron-down"></span></a>
+					</div>
+				</div>
+			</article>
+
+		<!-- Six -->
+			<article id="six" class="post invert style3 alt">
+				<div class="image">
+					<img src="images/pic12.jpg" alt="" data-position="80% center" />
+				</div>
+				<div class="content">
+					<div class="inner">
+						<header>
+							<h2><a href="generic.html">AWS Cloud Computing Services</a></h2>
+							<p class="info">x<a href="#">x</a></p>
+						</header>
+						<p>xxx</p>
+						<ul class="actions">
+							<li><a href="generic.html" class="button alt">Read More</a></li>
+						</ul>
+					</div>
+					<div class="postnav">
+						<a href="#five" class="scrolly prev"><span class="icon fa-chevron-up"></span></a>
+						<a href="#" class="scrolly next disabled"><span class="icon fa-chevron-down"></span></a>
+					</div>
+				</div>
+			</article>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+				</ul>
+				<div class="copyright">
+					&copy; TGK. All right limited to: <a href="https://techgenie.co.ke"></a>. 
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
